c3afb12bbb439f17e97df3dee721503e31bc18d0
Razaul Hridoy <imrazaulhridoy@gmail.com>
2021-11-18 10:21:23 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,276 @@
+(function ($) {
+"use strict";
+
+// meanmenu
+$('#mobile-menu').meanmenu({
+	meanMenuContainer: '.mobile-menu',
+	meanScreenWidth: "992"
+});
+
+// One Page Nav
+var top_offset = $('.header-area').height() - 10;
+$('.main-menu nav ul').onePageNav({
+	currentClass: 'active',
+	scrollOffset: top_offset,
+});
+
+
+$(window).on('scroll', function () {
+	var scroll = $(window).scrollTop();
+	if (scroll < 245) {
+		$(".header-sticky").removeClass("sticky");
+	} else {
+		$(".header-sticky").addClass("sticky");
+	}
+});
+
+
+
+// mainSlider
+function mainSlider() {
+	var BasicSlider = $('.slider_active');
+	BasicSlider.on('init', function (e, slick) {
+		var $firstAnimatingElements = $('.single_slider:first-child').find('[data-animation]');
+		doAnimations($firstAnimatingElements);
+	});
+	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+		var $animatingElements = $('.single_slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+		doAnimations($animatingElements);
+	});
+	BasicSlider.slick({
+		autoplay: false,
+		autoplaySpeed: 10000,
+		dots: false,
+		fade: true,
+		arrows: false,
+		responsive: [
+			{ breakpoint: 767, settings: { dots: false, arrows: false } }
+		]
+	});
+
+	function doAnimations(elements) {
+		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		elements.each(function () {
+			var $this = $(this);
+			var $animationDelay = $this.data('delay');
+			var $animationType = 'animated ' + $this.data('animation');
+			$this.css({
+				'animation-delay': $animationDelay,
+				'-webkit-animation-delay': $animationDelay
+			});
+			$this.addClass($animationType).one(animationEndEvents, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+}
+mainSlider();
+
+
+// Slick_slider
+
+$('.service_active').slick({
+	dots: false,
+	arrows: true,
+	autoplay:false,
+	infinite: true,
+	prevArrow:'<button type="button" class="slick-prev"><i class="fas fa-arrow-left"></i></button>',
+	nextArrow:'<button type="button" class="slick-next"><i class="fas fa-arrow-right"></i></button>',
+	speed: 300,
+	slidesToShow: 4,
+	slidesToScroll: 4,
+	responsive: [
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 3,
+		  slidesToScroll: 3,
+		  infinite: true,
+		  dots: false
+		}
+	  },
+	  {
+		breakpoint: 600,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 2
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+	  // You can unslick at a given breakpoint now by adding:
+	  // settings: "unslick"
+	  // instead of a settings object
+	]
+  });
+
+  
+// brand-active-slider
+$('.brand_active').slick({
+	dots: false,
+	autoplay:true,
+	infinite: true,
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	prevArrow:'<button type="button" class="slick-prev"><i class="far fa-arrow-alt-circle-left"></i></button>',
+	nextArrow:'<button type="button" class="slick-next"><i class="far fa-arrow-alt-circle-right"></i></button>',
+	arrows:  false,
+	responsive: [
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 4,
+		  slidesToScroll: 4,
+		  infinite: true,
+		  dots: false
+		}
+	  },
+	  {
+		breakpoint: 600,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 2
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+	 
+	]
+  });
+
+  
+// Slick_slider
+
+$('.servies_provied_active').slick({
+	dots: false,
+	arrows: true,
+	autoplay:false,
+	infinite: true,
+	prevArrow:'<button type="button" class="slick-prev"><i class="fas fa-arrow-left"></i></button>',
+	nextArrow:'<button type="button" class="slick-next"><i class="fas fa-arrow-right"></i></button>',
+	speed: 300,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	responsive: [
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 3,
+		  slidesToScroll: 3,
+		  infinite: true,
+		  dots: false
+		}
+	  },
+	  {
+		breakpoint: 600,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 2
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+	  // You can unslick at a given breakpoint now by adding:
+	  // settings: "unslick"
+	  // instead of a settings object
+	]
+  });
+
+// owlCarousel
+$('.testmonial_active').owlCarousel({
+    loop:true,
+	margin:30,
+	autoplay:true,
+	items:2,
+	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    nav:false,
+	dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        767:{
+            items:2
+        },
+        992:{
+            items:2
+        }
+    }
+})
+
+
+/* magnificPopup img view */
+$('.popup-image').magnificPopup({
+	type: 'image',
+	gallery: {
+	  enabled: true
+	}
+});
+
+/* magnificPopup video view */
+$('.play_btn').magnificPopup({
+	type: 'iframe'
+});
+
+
+// isotop
+$('.grid').imagesLoaded( function() {
+	// init Isotope
+	var $grid = $('.grid').isotope({
+	  itemSelector: '.grid-item',
+	  percentPosition: true,
+	  masonry: {
+		// use outer width of grid-sizer for columnWidth
+		columnWidth: '.grid-item',
+	  }
+	});
+});
+
+// filter items on button click
+$('.portfolio-menu').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+//for menu active class
+$('.portfolio-menu button').on('click', function(event) {
+	$(this).siblings('.active').removeClass('active');
+	$(this).addClass('active');
+	event.preventDefault();
+});
+
+
+
+
+// scrollToTop
+$.scrollUp({
+	scrollName: 'scrollUp', // Element ID
+	topDistance: '300', // Distance from top before showing element (px)
+	topSpeed: 300, // Speed back to top (ms)
+	animation: 'fade', // Fade, slide, none
+	animationInSpeed: 200, // Animation in speed (ms)
+	animationOutSpeed: 200, // Animation out speed (ms)
+	scrollText: '<i class="fas fa-arrow-up"></i>', // Text for element
+	activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+});
+
+// WOW active
+new WOW().init();
+
+
+})(jQuery);
